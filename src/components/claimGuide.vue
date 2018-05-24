152acfe71edbb25b42fc0a6b510e313ea9f30936
CallmeJay <291977464@qq.com>
2018-05-24 11:31:07 +0800

    理赔指引增加 添加功能

--- src/components/claimGuide.vue
+++ src/components/claimGuide.vue
@@ -2,7 +2,7 @@
   <div class="claim-guide">
     <form class="search-zone">
         <span>公司:</span>
-        <el-select v-model="companyCode" name="" placeholder="请选择公司">
+        <el-select v-model="companyCode" name="" placeholder="请选择公司" size="small">
         <el-option
             v-for="item in companyOptions"
             :key="item.name"
@@ -11,7 +11,7 @@
         </el-option>
         </el-select>
         <span>省份:</span>
-        <el-select v-model="provinceCode" name="" placeholder="请选择开通省份">
+        <el-select v-model="provinceCode" name="" placeholder="请选择开通省份" size="small">
         <el-option
             v-for="item in provinceOptions"
             :key="item.name"
@@ -20,7 +20,7 @@
         </el-option>
         </el-select>
         <span>城市:</span>
-        <el-select v-model="cityCode" name="" placeholder="请选择城市">
+        <el-select v-model="cityCode" name="" placeholder="请选择城市" size="small">
         <el-option
             v-for="item in cityOptions"
             :key="item.name"
@@ -28,8 +28,9 @@
             :value="item.id">
         </el-option>
         </el-select>
-        <span class="search-btn" @click="search">搜索</span>
+        <span class="search-btn" @click="search(1)">搜索</span>
         <span class="search-btn2" @click="clearInput">清空</span>
+        <span class="search-btn"  style="float:right;margin-top:15px;" @click="isCreate = true;">新增</span>              
     </form>
 
       <div class="caseListTable" v-if="totalCount">
@@ -66,7 +67,7 @@
       <div class="caseListTable" v-else>
         <p style="text-align:center;padding: 15px 0;">暂无数据</p>
       </div>
-
+    <!-- 编辑弹窗 -->
     <div class="mask" v-show="isPopup">
         <div class="popup">
             <b class="el-icon-circle-close-outline" @click='isPopup = false;'></b>
@@ -83,6 +84,52 @@
             </div>
         </div>
     </div>
+    <!-- 新增弹窗 -->
+    <div class="mask" v-show="isCreate">
+        <div class="popup">
+            <b class="el-icon-circle-close-outline" @click='close'></b>
+            <form class="search-zone" style="width: 100%;">
+                <span>公司:</span>
+                <el-select v-model="companyCode1" name="" placeholder="请选择公司">
+                <el-option
+                    v-for="item in companyOptions1"
+                    :key="item.name"
+                    :label="item.name"
+                    :value="item.code">
+                </el-option>
+                </el-select>
+                <span>省份:</span>
+                <el-select v-model="provinceCode1" name="" placeholder="请选择开通省份">
+                <el-option
+                    v-for="item in provinceOptions1"
+                    :key="item.name"
+                    :label="item.name"
+                    :value="item.id">
+                </el-option>
+                </el-select><br>
+                <span>城市:</span>
+                <el-select v-model="cityCode1" name="" placeholder="请选择城市">
+                <el-option
+                    v-for="item in cityOptions1"
+                    :key="item.name"
+                    :label="item.name"
+                    :value="item.id">
+                </el-option>
+                </el-select>            
+            </form>
+            <h3>请输入理赔指引内容:</h3>
+            <el-input
+                type="textarea"
+                :autosize="{ minRows: 8}"
+                placeholder="请输入理赔指引内容"
+                v-model.trim="textVal1">
+            </el-input>
+            <div class="btns">
+                <el-button type="success" @click='create' :disabled="isDisabled1">提交</el-button>
+                <el-button @click='clear1'>清空</el-button>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -93,31 +140,41 @@ export default {
     name: 'claimGuide',
   data (){
       return {
+            isCreate: false, // 是不是新增
             phone: '',
             textVal: '',
+            textVal1: '', // 新增
             isPopup: false, // 弹窗是否显示
             code: localStorage.insurecompanyCode,
-            isDisabled: false,
+            isDisabled: true,
+            isDisabled1: true,
             ajaxUrl: "/boot-pub-survey-manage",
             content: '',
-            companyCode: '',
+            tableData: [],
+            companyCode: '', // 搜索内容以下
             provinceCode: '',
             cityCode: '',
             companyOptions: [],
             provinceOptions: [],
             cityOptions: [],
-            tableData: [],
+            companyCode1: '', // 新增弹窗以下
+            provinceCode1: '',
+            cityCode1: '',
+            companyOptions1: [],
+            provinceOptions1: [],
+            cityOptions1: [],
             page: 1,
             pageSize: 10,
             totalCount: 0,
             currentId: '',
+            searchPage: 1,
 
       }
   },
 
   mounted (){
         // 加载列表
-        this.init();
+        this.search();
 
         // 加载公司列表
         var paramData = {
@@ -127,6 +184,7 @@ export default {
         .then( res => {
             console.log('公司',res.data.data.company)
             this.companyOptions = res.data.data.company;
+            this.companyOptions1 = res.data.data.company;
         })
 
         // 加载省份
@@ -134,6 +192,7 @@ export default {
         .then(response => {
             if(response.data.rescode == 200){
                 this.provinceOptions = response.data.data.provinces
+                this.provinceOptions1 = response.data.data.provinces
             }else{
                 this.$message({
                     showClose: true,
@@ -153,6 +212,13 @@ export default {
               this.isDisabled = true;
           }
       },
+      textVal1 (){
+          if(this.textVal1){
+            this.isDisabled1 = false;
+          }else {
+              this.isDisabled1 = true;
+          }
+      },
         provinceCode(){
             this.cityCode = '';
           if(this.provinceCode){
@@ -170,38 +236,56 @@ export default {
               }, err => {
                 console.log(err);
               })
-
+            }
+        },
+        provinceCode1(){
+            this.cityCode1 = '';
+          if(this.provinceCode1){
+            axios.get(this.ajaxUrl+"/pubsurvey/manage/department/v1/"+this.provinceCode1+"/citys")
+              .then(response => {
+                if(response.data.rescode == 200){
+                  this.cityOptions1 = response.data.data;
+                }else{
+                    this.$message({
+                        showClose: true,
+                        message: response.data.resdes,
+                        type: 'warning'
+                    });
+                }
+              }, err => {
+                console.log(err);
+              })
             }
         },
   },
   methods:{
       // 加载列表
-        init () {
-            axios.post(this.ajaxUrl+'/claims_guidelines/v2/page',{
-                currentPageNo: this.page,
-                pageSize: this.pageSize
-            }).then( res => {
-                console.log(res.data)
-                if(res.data.rescode == 200){
-                    this.totalCount = res.data.data.total;
-                    this.tableData = res.data.data.records;
-                }else {
-                    this.$message({
-                        showClose: true,
-                        message: res.data.resdes,
-                        type: 'warning'
-                    });
-                }
-            })
-        },
+        // init () {
+        //     axios.post(this.ajaxUrl+'/claims_guidelines/v2/page',{
+        //         currentPageNo: this.page,
+        //         pageSize: this.pageSize
+        //     }).then( res => {
+        //         console.log(res.data)
+        //         if(res.data.rescode == 200){
+        //             this.totalCount = res.data.data.total;
+        //             this.tableData = res.data.data.records;
+        //         }else {
+        //             this.$message({
+        //                 showClose: true,
+        //                 message: res.data.resdes,
+        //                 type: 'warning'
+        //             });
+        //         }
+        //     })
+        // },
       // 搜索
-      search() {
-          if(this.companyCode || this.provinceCode || this.cityCode){
+      search(n) {
+        //   if(this.companyCode || this.provinceCode || this.cityCode){
             const data = {
                 "insureCode": this.companyCode,
                 "cityCode": this.cityCode,
                 "provCode": this.provinceCode,
-                "currentPageNo": 1,
+                "currentPageNo": n ? n : this.searchPage,
                 "pageSize": 10
             };
             axios.post(this.ajaxUrl+'/claims_guidelines/v2/page',data)
@@ -218,7 +302,7 @@ export default {
                     });
                 }
             }) 
-          }
+        //   }
       },
 
       // 清空并刷新
@@ -227,8 +311,10 @@ export default {
           this.companyCode = '';
           this.provinceCode = '';
           this.cityOptions = [];
-          this.page = 1;
-          this.init();
+          this.textVal1 = '';
+        //   this.page = 1;
+          this.searchPage = 1;
+          this.search();
       },
 
     // 点击编辑
@@ -239,8 +325,51 @@ export default {
     },
 
     handleCurrentChange (val) {
-        this.page = val;
-        this.init();
+        // this.page = val;
+        this.searchPage = val;
+        this.search();
+    },
+    // 新增
+    create (){
+        if(!this.companyCode1){
+            return this.$message.error('请选择公司')
+        }
+        if(!this.provinceCode1){
+            this.$message.error("请选择公司所在省份");            
+        }
+        if(!this.cityCode1){
+            this.$message.error('请选择公司所在城市');            
+        }
+
+        axios.post(this.ajaxUrl+'/claims_guidelines/v2/add',{
+            "insureCode": this.companyCode1,
+            "cityCode": this.cityCode1,
+            "provCode": this.provinceCode1,
+            content: this.textVal1
+        }).then (res => {
+            if(res.data.rescode == 200){
+                this.clearInput();
+
+                this.$message({
+                showClose: true,
+                message: '创建成功',
+                type: 'success'
+                });
+                this.close();
+
+            }else {
+                this.$message.error(res.data.resdes);
+            }
+        })
+    },
+    // 关闭弹窗
+    close (){
+          this.cityCode1 = '';
+          this.companyCode1 = '';
+          this.provinceCode1 = '';
+          this.cityOptions1 = [];
+          this.textVal1 = '';
+          this.isCreate = false;
     },
     // 提交编辑
     submit(){
@@ -251,7 +380,8 @@ export default {
         .then(res => {
             console.log(res.data);
             if(res.data.rescode == 200){
-                this.init();
+                // 回到搜索装态
+                this.search();
                 this.$message({
                 showClose: true,
                 message: '提交成功',
@@ -259,16 +389,20 @@ export default {
                 });
                 this.isPopup = false;
             }else {
-                this.$message({
-                showClose: true,
-                message: res.data.resdes,
-                type: 'warning'
-                });
+                this.$message.error(res.data.resdes);
             }
         })
     },
     clear() {
         this.textVal = '';
+    },
+    // 新增的清空
+    clear1(){
+        this.textVal1 = '';
+        this.cityCode1 = '';
+        this.companyCode1 = '';
+        this.provinceCode1 = '';
+        this.cityOptions1 = [];
     }
   }
 }
@@ -346,8 +480,8 @@ export default {
         color: #fff;
         border-radius: 5px;
         background: #2eab3b;
-        padding: 8px 30px;
-        margin-left: 10px;
+        padding: 7px 20px;
+        margin-left: 5px;
         cursor: pointer;
     }
     .search-btn2 {
@@ -356,8 +490,8 @@ export default {
         color: #2eab3b;
         border-radius: 5px;
         border: 1px solid #2eab3b;
-        padding: 8px 30px;
-        margin-left: 10px;
+        padding: 7px 20px;
+        margin-left: 5px;
         cursor: pointer;
     }
 .caseListTable{
